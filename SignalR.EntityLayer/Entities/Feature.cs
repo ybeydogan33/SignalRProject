4489709f8bea4ea15017b6e38968b1183e9d4a16
Yavuz Beydoğan <y.beydogan33@gmail.com>
2026-04-06 18:30:01 +0300

    Entitylerin oluşturulması

---
+++ SignalR.EntityLayer/Entities/Feature.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalRApi;
+
+public class Feature
+{
+    public int FeaturId { get; set; }
+    public string ImageUrl { get; set; }
+    public string Title1 { get; set; }
+    public string Description1 { get; set; }
+    public string Title2 { get; set; }
+    public string Description2 { get; set; }
+    public string Title3 { get; set; }
+    public string Description3 { get; set; }
+
+}
---
+++ SignalR.EntityLayer/Entities/SocialMedia.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer;
+
+public class SocialMedia
+{
+public int SocialMediaID { get; set; }
+public string Name { get; set; }
+public string Url { get; set; }
+public string Icon { get; set; }
+}
